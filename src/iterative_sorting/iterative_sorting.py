2bd8832db4c4dc3b6c48b301b8824976bb628a5a
Mayankho <yankhot96@gmail.com>
2020-06-04 22:17:54 -0400

    Day one Mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,11 +1,17 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
+    #With the built in range function we dont need to explicitly write 0.
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-            for i in range(smallest_index, len(arr) - 1):
+        for j in range(cur_index + 1, len(arr)):
                 
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j 
+
+
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
@@ -19,10 +25,38 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+
+    init = range(len(arr) - 1)
+
+    for i in init: 
+        #Starts at 0
+        for j in init: 
+            #Compare values
+            if arr[i] > arr[j + 1]:
+                #Perform the swap
+                arr[i], arr[j] = arr[j] arr[i]
+            
 
 
+    """
+    made_a_swap = True
+    # We say  it's true so we can initiate the while loop
+    while made_a_swap:
+        made_a_swap = False 
+
+        #Create counters 
+        for i in range(0, len(arr) - 1):
+            j = i + 1
+
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+                made_a_swap = Trued
+    """
+# i  j
+#[1, 5, 2, 7]
     return arr
+
+
 
 '''
 STRETCH: implement the Count Sort function below
@@ -38,7 +72,7 @@ we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
 showed up, we can construct a sorted set of the input data from the 
 buckets. 
-
+ 
 What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
